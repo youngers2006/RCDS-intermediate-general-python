20a601631ee9441d475c5b2a7978267d27b8909f
youngers2006 <sy2224@ic.ac.uk>
2025-01-28 01:27:35 +0000

    Update distance_conversion.py

--- 01 Assert Statements/Exercise-Distance Conversion/distance_conversion.py
+++ 01 Assert Statements/Exercise-Distance Conversion/distance_conversion.py
@@ -6,3 +6,22 @@ def metres_to_miles(dist_metres):
     dist_miles = 0.621371 * dist_km
 
     return (dist_miles)
+
+def function_tester(d1,d2,d3):
+    dm1 = meters_to_miles(d1)
+
+    assert dm1 == 1.242742, "dm1 incorrect"
+    
+    dm2 = meters_to_miles(d2)
+
+    assert dm2 == 0.621371, "dm2 incorrect"
+
+    
+    dm3 = meters_to_miles(d3)
+
+    assert dm3 == 0.9320565, "dm3 incorrect"
+
+
+
+
+
